0d90a1016f9ae2eb69a29c78271c749f9cad1ab1
SneakySnake235 <jacobpickrell@gmail.com>
2019-10-25 10:49:56 -0600

    HRr

--- cit_230/assignments/lesson6/scripts/windchill.js
+++ cit_230/assignments/lesson6/scripts/windchill.js
@@ -1,7 +1,19 @@
 let currentTemp = 41;
 let windSpeed = 8;
-let windChill =35.74 + 0.6215 * currentTemp;
+let windChill = 0;
 
-document.getElementById("currentTemp").innerHTML=
-document.getElementById("windSpeed").innerHTML=
-document.getElementById("wnidChill").innerHTML=
+if (currentTemp < 50 && windSpeed > 3) {
+
+  windChill =35.74 + 0.6215 * currentTemp - 35.75 *
+     Math.pow(windSpeed, 0.16) + 0.4275 * currentTemp * Math.pow(windSpeed, 0.16);
+}
+document.getElementById("currentTemp").innerHTML= currentTemp + '&deg;';
+document.getElementById("windSpeed").innerHTML= windSpeed + 'mph';
+
+if (windChill === 0) {
+    document.getElementById("windChill").innerHTML= "N/A";
+
+}
+else{
+    document.getElementById("windChill").innerHTML= Math.round(windChill) + '&deg;';
+}
