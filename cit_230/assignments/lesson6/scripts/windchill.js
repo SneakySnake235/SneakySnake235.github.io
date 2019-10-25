4dc5705b5024ecb8b438d8614b6d7641170af22c
SneakySnake235 <jacobpickrell@gmail.com>
2019-10-25 10:22:47 -0600

    skrrt skkrrt

---
+++ cit_230/assignments/lesson6/scripts/windchill.js
@@ -0,0 +1,7 @@
+let currentTemp = 41;
+let windSpeed = 8;
+let windChill =35.74 + 0.6215 * currentTemp;
+
+document.getElementById("currentTemp").innerHTML=
+document.getElementById("windSpeed").innerHTML=
+document.getElementById("wnidChill").innerHTML=
