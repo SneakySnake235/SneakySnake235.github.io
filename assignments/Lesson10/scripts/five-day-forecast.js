452f8c7bd7ca01c8d4bbc5f7568a416cb2598060
SneakySnake235 <jacobpickrell@gmail.com>
2020-07-02 12:51:43 -0600

    fix

--- assignments/Lesson10/scripts/five-day-forecast.js
+++ assignments/Lesson10/scripts/five-day-forecast.js
@@ -1,17 +1,3 @@
-//5 Day Forecast looping days
-let abbrw_names = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thur'];
-let dayone = abbrw_names[d.getDay()+1];
-let daytwo = abbrw_names[d.getDay()+2];
-let daythree = abbrw_names[d.getDay()+3];
-let dayfour = abbrw_names[d.getDay()+4];
-let dayfive = abbrw_names[d.getDay()+5];
-console.log(dayfive);
-
-document.getElementById("day1").innerHTML = dayone;
-document.getElementById("day2").innerHTML = daytwo;
-document.getElementById("day3").innerHTML = daythree;
-document.getElementById("day4").innerHTML = dayfour;
-document.getElementById("day5").innerHTML = dayfive;
 
 
 //Weather Summary content
@@ -25,7 +11,8 @@ fetch(apiURL)
     (currentWeather) => {
        console.log(currentWeather);
 
-let description = currentWeather.weather.description;
+// let description = currentWeather.weather.description;
+
 let currentTemp = currentWeather.main.temp;
 let windSpeed = currentWeather.wind.speed;
 let humidity = currentWeather.main.humidity;
@@ -35,12 +22,13 @@ if (currentTemp < 50 && windSpeed > 3) {
     windChill = 35.74 + 0.6215 * currentTemp - 35.75 * Math.pow(windSpeed,0.16)
                  + 0.427 * currentTemp * Math.pow(windSpeed);
 }
+// document.getElementById("description").innerHTML = description;
 
-document.getElementById("currentTemp").innerHTML = currentTemp + '&deg';
-document.getElementById("description").innerHTML = description;
-document.getElementById("humidity").innerHTML = humidity + '%';
-document.getElementById("windSpeed").innerHTML = windSpeed + 'mph';
-document.getElementById("windChill").innerHTML = windChill + '&deg';
+
+document.getElementById("currentTemp").innerHTML = FinalTemp + "&deg;";
+        document.getElementById("tempHigh").innerHTML = FinalDeg + "&deg;";
+        document.getElementById("windSpeed").innerHTML = windSpeed + " mph";
+        document.getElementById("windChill").innerHTML = windChill;
 if (windChill === 0) {
     document.getElementById("windChill").innerHTML = 'N/A';
 }
@@ -49,4 +37,9 @@ else {
 }
 
 }
+
+
+
+
+
 );
